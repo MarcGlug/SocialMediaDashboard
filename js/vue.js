3272ed2888d9bed75dd1192c1f2b47581a54836a
MarcGlug <marc.boyon@hotmail.fr>
2020-11-03 21:00:27 +0100

    MEP responsive

--- js/vue.js
+++ js/vue.js
@@ -1,7 +1,7 @@
 Vue.component('social-card', {
     props: ['social'],
     template: `
-        <div class="text-center card mx-auto mb-6">
+        <div class="text-center card mx-auto">
             <div class="flex justify-center">
                 <img :src="social.logo" alt="logo facebook">  
                 <p class="ml-3 text-bold">{{social.pseudo}}</p>
@@ -12,12 +12,11 @@ Vue.component('social-card', {
             </div>
             <div class="flex justify-center stat">
                 <img :src="social.arrow" alt="arrow up">
-                <p><span>{{social.evolution}}</span> Today</p>
+                <p>{{social.evolution}} Today</p>
             </div>
         </div>
     `
 })
-
 
 var socialCard = new Vue({
     el: '#social-section',
@@ -56,5 +55,92 @@ var socialCard = new Vue({
                 evolution:'144'
             }
         ]
+    }
+})
+
+// ------------------------------------- OVERVIEW ------------------------------------------
+
+
+var overviewCard = new Vue({
+    el:"#overview-section",
+    data: {
+        socials: [
+            {
+                socialNetwork: [
+                    {
+                        id:0,
+                        title: "Page Views",
+                        logo: "images/icon-facebook.svg",
+                        stat: "87",
+                        evolution: "+3"
+                    },
+                    {
+                        id:1,
+                        title: "Likes",
+                        logo: "images/icon-facebook.svg",
+                        stat: "52",
+                        evolution: "-2"
+                    }
+                ]
+            },
+            {
+                socialNetwork: [
+                   {
+                       id:0,
+                       title: "Profile Views",
+                       logo: "images/icon-instagram.svg",
+                       stat: "52k",
+                       evolution: "+1375"
+                   },
+                   {
+                       id:1,
+                       title: "Likes",
+                       logo: "images/icon-instagram.svg",
+                       stat: "5462",
+                       evolution: "+2257"
+                   }
+                ]
+            },
+            {
+                socialNetwork: [
+                {
+                    id:0,
+                    title: "Retweets",
+                    logo: "images/icon-twitter.svg",
+                    stat: "117",
+                    evolution: "+303"
+                },
+                {
+                    id:1,
+                    title: "Likes",
+                    logo: "images/icon-twitter.svg",
+                    stat: "507",
+                    evolution: "+553"
+                }
+             ]
+            },
+            {
+                socialNetwork: [
+                {
+                    id:0,
+                    title: "Total Views",
+                    logo: "images/icon-youtube.svg",
+                    stat: "1407",
+                    evolution: "-12"
+                },
+                {
+                    id:1,
+                    title: "Likes",
+                    logo: "images/icon-youtube.svg",
+                    stat: "107",
+                    evolution: "-12"
+                }
+             ],
+
+            }
+        ]
+    },
+    method:{
+
     }
 })
